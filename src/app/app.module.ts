b74b3b87ab1c507804c4d2180987ca5a05647886
michaelwood1996 <44974781+michaelwood1996@users.noreply.github.com>
2019-02-21 10:54:20 +0000

    rwer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { ProductPageComponent } from
 import { ProductDescriptionComponent } from 
 './product-description/product-description.component';
 import { ProductService } from './product.service';
+import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductPageComponent,
-    ProductDescriptionComponent
+    ProductDescriptionComponent,
+    ProductTracklistingComponent
   ],
   imports: [
     BrowserModule,
